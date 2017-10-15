a408eef04011c202c61c00182bc4dd56845b33e1
Mitul Patel <mitulp91@gmail.com>
2017-10-15 11:55:39 -0400

    clean up film data constants to allow them to be exported separately; this will allow easy rendering of single films outside of a list for purposes such as a featured film

--- src/components/Films/films-data.js
+++ src/components/Films/films-data.js
@@ -1,34 +1,40 @@
-const films = [
-    {
-        id: 179046995,
-        description: 'Catch of the Day',
-        credits: [
-            {
-                name: 'Rishabh Patel',
-                title: 'Director',
-            },
-        ],
-    },
-    {
-        id: 179365776,
-        description: 'Good Times, Bad Times',
-        credits: [
-            {
-                name: 'Rishabh Patel',
-                title: 'Director',
-            },
-        ],
-    },
-    {
-        id: 179356990,
-        description: 'L.M.N',
-        credits: [
-            {
-                name: 'Rishabh Patel',
-                title: 'Director',
-            },
-        ],
-    }
+export const FILM_CATCH_OF_THE_DAY = {
+    id: 179046995,
+    description: 'Catch of the Day',
+    credits: [
+        {
+            name: 'Rishabh Patel',
+            title: 'Director',
+        },
+    ],
+};
+
+export const FILM_GOOD_TIMES_BAD_TIMES = {
+    id: 179365776,
+    description: 'Good Times, Bad Times',
+    credits: [
+        {
+            name: 'Rishabh Patel',
+            title: 'Director',
+        },
+    ],
+};
+
+export const FILM_LMN = {
+    id: 179356990,
+    description: 'L.M.N',
+    credits: [
+        {
+            name: 'Rishabh Patel',
+            title: 'Director',
+        },
+    ],
+};
+
+export const FILMS = [
+    FILM_CATCH_OF_THE_DAY,
+    FILM_GOOD_TIMES_BAD_TIMES,
+    FILM_LMN,
 ];
 
-export default films;
+export default FILMS;
